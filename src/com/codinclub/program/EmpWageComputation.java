63e5b31cbc73c6c7f9dc782d17933374d2fbf9e3
anujranjan3 <ararky7@hmail.com>
2020-10-21 11:45:30 +0530

    Code to find Employee Wage for Multiple Companies

--- src/com/codinclub/program/EmpWageComputation.java
+++ src/com/codinclub/program/EmpWageComputation.java
@@ -1,6 +1,8 @@
 package com.codinclub.program;
 import com.codinclub.utilities.*;
 
+import java.util.Scanner;
+
 /*
  * @author Anuj Ranjan Kumar
  * Create Date: 19th October 2020
@@ -12,14 +14,30 @@ public class EmpWageComputation {
 
     public static void main (String []args){
 
+        Scanner scanner=new Scanner(System.in);
         System.out.println("Welcome to Employee Wage Problem ");
         Utility utilities=new Utility();
         EmpObjects empObjects=new EmpObjects();
-        empObjects.setCompany("Dmart");
-        empObjects.setEmpRatePerHr(20);
-        empObjects.setNumWorkingDays(20);
-        empObjects.setMaxWorkingHrsInMonth(100);
-        utilities.attendance(empObjects.getCompany(),empObjects.getEmpRatePerHr(),empObjects.getNumWorkingDays(),empObjects.getMaxWorkingHrsInMonth());
+        System.out.println("Enter Number of Companies to be computed :");
+        int counter=scanner.nextInt();
+
+        for (int i=0; i<counter; i++){
+
+            System.out.println("Enter Company Name :");
+            String company = scanner.next();
+            System.out.println("Employee Rate Per Hour :");
+            int empRatePerHr = scanner.nextInt();
+            System.out.println("Enter Maximum  Number of Working Days in a Month a employee should work :");
+            int NumWorkingDays = scanner.nextInt();
+            System.out.println("Enter Maximum Hours a employee should work in a month :");
+            int setMaxWorkingHrsInMonth = scanner.nextInt();
+            empObjects.setCompany(company);
+            empObjects.setEmpRatePerHr(empRatePerHr);
+            empObjects.setNumWorkingDays(NumWorkingDays);
+            empObjects.setMaxWorkingHrsInMonth(setMaxWorkingHrsInMonth);
+            utilities.attendance(empObjects.getCompany(),empObjects.getEmpRatePerHr(),empObjects.getNumWorkingDays(),empObjects.getMaxWorkingHrsInMonth());
+
+        }
 
     }
 }
